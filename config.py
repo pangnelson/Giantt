b86bbf34079e51538e105ad17de183f641f65639
pangnelson <87406261+pangnelson@users.noreply.github.com>
2023-05-04 12:01:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-customhost = "giant-db.cnrk0qqjw3cb.us-east-1.rds.amazonaws.com"
-customuser = "giant_user"
+customhost = "giant-db.c7fbjsirefag.us-east-1.rds.amazonaws.com"
+customuser = "giant_main"
 custompass = "11223344"
 customdb = "giantdb"
 customregion = "us-east-1"
